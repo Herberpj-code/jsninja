adb27dcb1520b0359bcbb3cf8f22a21c6ca2c641
Herber Pucinelli Jr <herberpj.code@gmail.com>
2021-04-02 13:11:25 -0300

    Resolvendo parcialmente os exercicios e explicando nos comentários o por que nao consegui resolve-los / Solving exercises partially and explaining why in the comments file

---
+++ aula 5/class 05.js
@@ -0,0 +1,99 @@
+/*
+Crie uma variável qualquer, que receba um array com alguns valores aleatórios
+- ao menos 5 - (fica por sua conta os valores do array).
+*/
+var array = [1, 2, 3, 4, 5]
+
+/*
+Crie uma função que receba um array como parâmetro, e retorne esse array.
+*/
+function arrayFunction(array) {
+     return array;
+}
+
+/*
+Imprima o segundo índice do array retornado pela função criada acima.
+*/
+arrayFunction(array)[2]; // 3
+
+/*
+Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
+segundo, um número. A função deve retornar o valor de um índice do array que foi passado
+no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
+segundo parâmetro.
+*/
+function arrayFunction2(array, num) {
+     return array[num];
+}
+
+/*
+Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
+*/
+var newArray = [1, null, undefined, 'herber', true];
+
+/*
+Invoque a função criada acima, fazendo-a retornar todos os valores do último
+array criado.
+*/
+arrayFunction(newArray); // [ 1, null, undefined, 'herber', true ]
+
+/*
+Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
+livro. Dentro dessa função, declare uma variável que recebe um objeto com as
+seguintes características:
+- esse objeto irá receber 3 propriedades, que serão nomes de livros;
+- cada uma dessas propriedades será um novo objeto, que terá outras 3
+propriedades:
+    - `quantidadePaginas` - Number (quantidade de páginas)
+    - `autor` - String
+    - `editora` - String
+- A função deve retornar o objeto referente ao livro passado por parâmetro.
+- Se o parâmetro não for passado, a função deve retornar o objeto com todos
+os livros.
+*/
+
+//My Function
+function book(livro) {
+	var livro = {
+		livro1: {
+			quantidadePaginas: 50,
+			autor: 'Herber',
+			editora: 'cia do saber'},
+		livro2: {
+			quantidadePaginas: 60,
+			autor: 'Jonas',
+			editora: 'letrinhas'},
+		livro3: {
+			quantidadePaginas: 70,
+			autor: 'Fernando',
+			editora: 'casa do codigo'}
+    }
+}
+
+//Instructor Function
+
+/*
+Usando a função criada acima, imprima o objeto com todos os livros.
+*/
+// ?
+
+/*
+Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
+usando a frase:
+"O livro [NOME_DO_LIVRO] tem [X] páginas!"
+*/
+// ?
+
+/*
+Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
+a frase:
+"O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
+*/
+// ?
+
+/*
+Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
+a frase:
+"O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
+*/
+// ?
---
+++ aula 5/comments05.md
@@ -0,0 +1,7 @@
+## Comments
+
+**Portuguese Version**
+Eu acredito que seja parte do processo de aprendizado, mas eu estou tendo bastante dificuldade para resolver os exercicios, as coisas ainda não estão claras pra mim, como eu consigo chamar objetos dentro de objetos passando por parametro na função, isso me confundiu bastante, só consegui fazer um rascunho e não consegui resolver, fiquei travado nesse exercicio, estou até me sentindo estranho, mas não vou desistir, vou fazer o que for preciso pra entender essas funções no meu tempo, mas por agora preciso ver a explicação para realmente aprender.
+
+**English Version**
+I think its part of the learning proccess but Im encountering some difficulties in solving the exercises, things are not clear to me on how can I call objects inside objects,  inside the function by passing it to paramaters in the function, it confused me a lot, I could only do a sketch and wasn't able to solve, I got stuck in this exercise, feeling awkward about it, but I won't give up so easily, whatever it takes I'll be able to understand this in my time, but from now I really need to see explanations from the instructor so I can learn it.
